9c03bcd2c02e56f1759ff27c57bdb51a28b9bef0
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-07-14 16:02:59 +0100

    completed the first stage of the app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import { Header } from './components/Header';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 
+
+import { GlobalProvider } from './context/GlobalState';
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className='container'>
         <Balance />
@@ -15,7 +18,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
