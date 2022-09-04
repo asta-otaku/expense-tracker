23db211e5da42a7d9b5754431f656664d2eb5c91
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-09-04 22:44:13 +0100

    Final Adjustments to the Expense Tracker App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import AddTransaction from './components/AddTransaction';
-import { Balance } from './components/Balance';
-import { Header } from './components/Header';
-import { IncomeExpenses } from './components/IncomeExpenses';
-import { TransactionList } from './components/TransactionList';
+import "./App.css";
+import AddTransaction from "./components/AddTransaction";
+import { Balance } from "./components/Balance";
+import { Header } from "./components/Header";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 
-
-import { GlobalProvider } from './context/GlobalState';
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
     <GlobalProvider>
       <Header />
-      <div className='container'>
+      <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,19 +1,22 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export default function Transaction({ transaction }) {
-const { deleteTransaction } = useContext(GlobalContext)
+  const { deleteTransaction } = useContext(GlobalContext);
 
-let StoredTransactions = []
-
-StoredTransactions.push(transaction)
-localStorage.setItem("TRANSACTIONS",JSON.stringify(StoredTransactions))
-
-const sign = transaction.amount < 0 ? '-': '+'
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
-    <li className={transaction.amount < 0 ? "minus": "plus"}>
-    {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-    <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
-  </li>
-  )
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
 }
